5d084c3371c287eee6327ff8a20a915b64a304a3
Evaristekanova <dusingizimanaevariste3@example.com>
2023-07-30 10:49:04 +0200

    setup library frontend web app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="text-3xl text-center font-light pt-4">
+        Online Library App
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center h-20 border shadow-md justify-between">
+      <div className="mx-16">Logo</div>
+      <div className="flex ml-[20rem] w-[28%] border py-1 px-2 rounded-md">
+        <input
+          type="text"
+          className="w-full h-full outline-none py-1"
+          placeholder="Search for books"
+        />
+        <button className="p-1 bg-green-900 rounded-sm text-white font-light">Search</button>
+      </div>
+      <div className="flex justify-around mr-20">
+        <Link to="/login" className="text-center mr-4">
+          Login
+        </Link>
+        <Link to="/register" className="text-center ml-4">
+          Register
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
