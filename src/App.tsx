846be0b1d24d9737977cf80fce6d9be8abc99640
Evaristekanova <dusingizimanaevariste3@gmail.com>
2023-08-22 00:40:21 +0200

    add unstyled landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import LANDING from "./assets/LANDING.jpg";
 const App = () => {
   return (
-    <div className="text-3xl text-center font-light pt-4 min-h-screen">
-      Online Library App
+    <div className="sm:flex sm:flex-col md:flex md:flex-row lg:bg-lightGreen lg:flex lg:flex-row sm:pt-3 lg:h-[100%] lg:pt-0 pt-2 flex flex-col relative">
+      <div className="sm:w-1/2 md:w-full lg:w-1/2 lg:flex lg:items-center lg:h-full lg:justify-center lg:border  md:h-full w-full">
+        Hello Here
+      </div>
+      <div className="sm:w-full md:w-1/2 lg:w-1/2 lg:flex lg:items-center lg:flex-grow  md:h-[90%] w-full">
+        <img
+          src={LANDING}
+          alt=""
+          className="sm:w-full md:w-full lg:w-full h-[90%] object-cover w-full"
+        />
+      </div>
     </div>
   );
 };
---
+++ src/Pages/Loading.ts
@@ -0,0 +1,11 @@
+// const Loading = () => {
+//   return (
+//     <div className="h-screen w-full flex justify-center items-center">
+//       <h1 className="animate-bounce text-blue-500 text-3xl md:text-2xl text-center">
+//         Loading...
+//       </h1>
+//     </div>
+//   );
+// };
+
+// export default Loading;
