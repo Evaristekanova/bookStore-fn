320df5291f22aae449f9a33b131f552415db6b4d
Evaristekanova <dusingizimanaevariste3@gmail.com>
2023-09-08 00:02:54 +0200

    add image as a placeholder on landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import ExploreBooks from "./components/ExploreBooks";
-// import LANDING from "./assets/LANDING.jpg";
+import LANDING from "./assets/LANDING.jpg";
 const App = () => {
   return (
     <>
       <div className="w-screeen h-auto bg-lightGreen text-white">
         <div className="flex flex-col items-center justify-center w-full">
-          <h1 className="text-4xl font-bold">Welcome to Online Book Store</h1>
-          <h2 className="text-2xl font-semibold">Expand Your Mind Wiht Our Books</h2>
+          <h1 className="text-3xl font-bold">Welcome to Online Book Store</h1>
+          <h2 className="text-xl font-semibold">Expand Your Mind Wiht Our Books</h2>
+          <img src={LANDING} alt="landing" className="w-1/2 h-auto" />
           </div>
       </div>
       <ExploreBooks />
