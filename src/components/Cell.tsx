5626fd00f202ae89ec4a7a15738d81a5494b475c
Evaristekanova <dusingizimanaevariste3@gmail.com>
2023-09-10 20:29:04 +0200

    customize table component

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,21 +1,22 @@
-
-interface CellProps { 
+interface CellProps {
   email: string;
   firstName: string;
   lastName: string;
   id: number;
   role: string;
 }
-const Cell = ({ email, firstName, lastName, id, role }: CellProps) => { 
+const Cell = ({ email, firstName, lastName, id, role }: CellProps) => {
   return (
     <>
-      <td className="border px-4 py-2">{id}</td>
-      <td className="border px-4 py-2">{firstName}</td>
-      <td className="border px-4 py-2">{lastName}</td>
-      <td className="border px-4 py-2">{email}</td>
-      <td className="border px-4 py-2">{role}</td>
+      <tr>
+        <td className="border px-4 py-2">{id}</td>
+        <td className="border px-4 py-2">{firstName}</td>
+        <td className="border px-4 py-2">{lastName}</td>
+        <td className="border px-4 py-2">{email}</td>
+        <td className="border px-4 py-2">{role}</td>
+      </tr>
     </>
   );
-}
+};
 
 export default Cell;
--- src/dashboard/Users.tsx
+++ src/dashboard/Users.tsx
@@ -1,10 +1,8 @@
-// import Table from "../components/Table";
-import TansTack from "../components/TansTack";
+import Table from "../components/Table";
 const Users = () => {
   return (
     <div className="flex flex-col w-[80vw] h-screen px-4 py-8">
-      {/* <Table /> */}
-      <TansTack />
+      <Table />
     </div>
   );
 };
